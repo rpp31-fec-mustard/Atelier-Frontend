ec2fb35232d1fbfa7d051f07d14ffe158d6c6639
Michael Lapid <19236670+addessosf@users.noreply.github.com>
2021-10-21 21:36:25 -0700

    webpack.config setup

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: __dirname + '/client/src/index.jsx',
+  module: {
+    rules: [
+      {
+        test: [/\.jsx$/],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  output: {
+    filename: 'bundle.js',
+    path: __dir + '/client/dist'
+  }
+};
