063e6f6a54055e37e77ea3d1b899846f4b92a527
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-10-23 13:08:08 -0700

    component setup complete

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+
+const App = () => {
+  return (
+    <div> Hello World! </div>
+  )
+};
 
 ReactDOM.render(<App />, document.getElementById('app'));
