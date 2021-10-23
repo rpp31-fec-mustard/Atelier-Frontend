942af264063491f257b3ac9d4ccc0c16e78681c6
Michael Lapid <19236670+addessosf@users.noreply.github.com>
2021-10-23 16:25:15 -0700

    Create temp widget containers.
    Temporary. Feel free to adjust as needed. Currently styled by .module_container.

---
+++ client/src/ProductOverview/ProductOverview.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const ProductOverview = () => (
+
+
+  <div id='product_overview_main'>
+    <h1>PRODUCT OVERVIEW WIDGET</h1>
+  </div>
+
+
+)
+
+
+
+
+
+
+
+
+export default ProductOverview
