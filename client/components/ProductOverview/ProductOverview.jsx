2d3a89c215364f07b50cdd74cf6b8514bc6d4f75
Michael Lapid <19236670+addessosf@users.noreply.github.com>
2021-10-25 15:21:38 -0700

    Temp top banner

--- client/components/ProductOverview/ProductOverview.jsx
+++ client/components/ProductOverview/ProductOverview.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 const ProductOverview = () => (
 
   <div id='product_overview_main' className="module_container">
-    <h1>PRODUCT OVERVIEW WIDGET</h1>
     <div className='top01'>
       <div className='left02'>
         <div className='thumbnails_po'>
