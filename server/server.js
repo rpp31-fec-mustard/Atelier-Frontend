e5c65e060b053d870bb57039fc59ac70e91b24e0
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-10-25 23:45:15 -0700

    Successful inital API call for related products using Axios

--- server/server.js
+++ server/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const parser = require('body-parser');
+const api = require('./apiHelpers.js');
 const app = express();
 
 app.use(express.static(__dirname + '/../client/public'));
 app.use(parser.json());
+
+app.get('/related', (req, res) => {
+  api.parseRelated('59553')
+    .then((related) => {
+      console.log('server', {related})
+      res.send('RELATED');
+    })
+});
 
 const port = 5500;
 app.listen(port, () => {
