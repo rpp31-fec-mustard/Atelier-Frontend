cfff0fe6a6a1ede6352cb9ed6652e4d14dfabcfa
Joey Dowling <Joeydowling27@gmail.com>
2021-10-26 12:56:20 -0700

    adjusted SortBy component

--- client/components/Reviews/SortBy.jsx
+++ client/components/Reviews/SortBy.jsx
@@ -1,23 +1,16 @@
 import React from 'react';
 
-class SortBy extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-  render() {
-    return (
-      <form>
-        <label> {this.props.list.length} reviews, Sorted by
-        </label>
-        <select value={this.props.selected} onChange={this.props.onChange} name="sortBy" className="sortBy">
-          <option>relevance</option>
-          <option>newest</option>
-          <option>most helpful</option>
-        </select>
-      </form>
-    );
-  }
+const SortBy = (props) => {
+  return (
+    <form>
+      <label> {props.list.length} reviews, Sorted by
+      </label>
+      <select value={props.selected} onChange={props.onChange} name="sortBy" className="sortBy">
+        <option>relevance</option>
+        <option>newest</option>
+        <option>most helpful</option>
+      </select>
+    </form>
+  );
 }
-
 export default SortBy;
