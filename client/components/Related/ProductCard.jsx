b49b1bdd0813212b18c0280b02af1f1c28bc6c20
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-10-28 20:18:39 -0700

    Star buttons now filling and unfilling

--- client/components/Related/ProductCard.jsx
+++ client/components/Related/ProductCard.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import StarButton from './StarButton.jsx';
-import XButton from './XButton.jsx';
+import React from "react";
+import StarButton from "./StarButton.jsx";
+import XButton from "./XButton.jsx";
 
 const ProductCard = (props) => {
   let actionButton;
 
-  if (props.action === 'star') {
-    actionButton = <StarButton handleStar={props.handleStar} starred={true}/>;
+  if (props.action === "star") {
+    actionButton = <StarButton handleStar={props.handleStar} starred={true} />;
   } else {
     actionButton = <XButton />;
   }
--- client/components/Related/Related.jsx
+++ client/components/Related/Related.jsx
@@ -1,36 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import RelatedProducts from './RelatedProducts.jsx';
-import YourOutfit from './YourOutfit.jsx';
+import RelatedProducts from "./RelatedProducts.jsx";
+import YourOutfit from "./YourOutfit.jsx";
 
 const Related = (props) => {
-  const [productId, setProductId] = useState('59553');
+  const [productId, setProductId] = useState("59553");
   const [relatedProducts, setRelatedProducts] = useState([]);
   // [{productId, starred, category, name, price, rating}]
   const [outfitList, setOutfitList] = useState([]);
   // [{productId, category, name, price, rating}]
 
   useEffect(() => {
-    axios.get('/related')
-    .then((result) => {
+    axios.get("/related").then((result) => {
       setRelatedProducts(result.data);
-    })
+    });
   }, []);
 
   function handleStar(event) {
     // fill in star
     // update outfit list with clicked on product info
-    console.log('star clicked', event.target);
+    console.log("star clicked", event.target);
   }
 
   return (
     <div id="related_main" className="module_container">
-      <RelatedProducts productId={productId} relatedProducts={relatedProducts} handleStar={handleStar}/>
+      <RelatedProducts
+        productId={productId}
+        relatedProducts={relatedProducts}
+        handleStar={handleStar}
+      />
       <YourOutfit />
     </div>
   );
-
 };
 
 export default Related;
