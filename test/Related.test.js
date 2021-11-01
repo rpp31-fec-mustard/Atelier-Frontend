f1869757877f8ed2bf8d430dc2a9a05df944dd6c
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-11-01 10:00:11 -0700

    Enzyme and sinon incorporated. Related tests rewritten using enzyme API

--- test/Related.test.js
+++ test/Related.test.js
@@ -1,43 +1,28 @@
 import React, { useState } from 'react';
-import { render, unmountComponentAtNode } from 'react-dom';
-import { act } from 'react-dom/test-utils';
+// import { render, unmountComponentAtNode } from 'react-dom';
+// import { act } from 'react-dom/test-utils';
+import { shallow, mount, render } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import sinon from 'sinon';
+
+configure({ adapter: new Adapter() });
 
 import Related from '../client/components/Related/Related.jsx';
 
-let container = null;
-beforeEach(() => {
-  // setup a DOM element as a render target
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  // cleanup on exiting
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
 describe('Related products module: render tests', () => {
   test('Module should render', () => {
-    act(() => {
-      render(<Related product={'59553'}/>, container);
-    });
-    const related = document.getElementById('related_main');
+    const related = shallow(<Related />);
     expect(!!related).toBe(true);
   });
 
-  test('4 product cards should render in related products', () => {
-    act(() => {
-      render(<Related />, container);
-    });
+  test('Four product cards should render in related products', () => {
+    const related = shallow(<Related />);
     const productCards = document.getElementsByClassName('prod-card');
     expect(productCards.length).toBe(4);
   });
 
   test('Empty product card should render in outfit list if empty', () => {
-    act(() => {
-      render(<Related />, container);
-    });
+    const related = shallow(<Related />);
   });
 });
