b6c652c0f6455e2de7e094070e2ffc5284c91797
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-10-31 21:22:23 -0700

    Hovering event working

--- client/components/Related/ProductCard.jsx
+++ client/components/Related/ProductCard.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import StarButton from './StarButton.jsx';
+import ProductComparison from './ProductComparison.jsx';
+
 const ProductCard = (props) => {
+  const [isHovering, setIsHovering] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
   let actionButton;
 
   if (!props.outfit) {
@@ -8,13 +19,21 @@ const ProductCard = (props) => {
   }
 
   return (
-    <div className={`prod-card ${props.id}`}>
-      <img src={props.image}></img>
-      <div>{props.category}</div>
-      <div>{props.name}</div>
-      <div>${props.price}</div>
-      <div>{props.rating}</div>
+    <div className={`prod-card ${props.id}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseOut}
+    >
+      <a href="https://xd.adobe.com/view/e600dc0f-454c-44e3-5075-7872d04189ff-9031/?fullscreen" target="_blank">
+        <div className="product-card-img-wrapper">
+          <img src={props.image} className="product-card-img"></img>
+        </div>
+        <div>{props.category}</div>
+        <div>{props.name}</div>
+        <div>${props.price}</div>
+        <div>{props.rating}</div>
+      </a>
       {actionButton}
+      {isHovering && <ProductComparison />}
     </div>
   );
 };
---
+++ client/components/Related/ProductComparison.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductComparison = (props) => {
+  return (
+    <div>
+      HOVER TEST
+    </div>
+  );
+};
+
+export default ProductComparison;
