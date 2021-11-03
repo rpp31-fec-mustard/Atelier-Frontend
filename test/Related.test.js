2d69737528ed40de5b4339e7ec5c1318a0a6115b
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-11-02 19:48:54 -0700

    Minor testing additions including coverage CLI command on jest script

--- test/Related.test.js
+++ test/Related.test.js
@@ -9,18 +9,20 @@ configure({ adapter: new Adapter() });
 import Related from '../client/components/Related/Related.jsx';
 
 describe('Related products module: render tests', () => {
+  const related = shallow(<Related />);
   test('Module should render', () => {
-    const related = shallow(<Related />);
     expect(!!related).toBe(true);
   });
 
+  test('Related products and Outfit submodules should render', () => {
+  });
+
   test('At most four product cards should render in related products submodule', () => {
-    const related = shallow(<Related />);
+    // const related = shallow(<Related />);
     const productCards = document.getElementsByClassName('prod-card');
     expect(productCards.length).toBe(4);
   });
 
-  test('Empty product card should render in outfit list if empty', () => {
-    const related = shallow(<Related />);
+  test('Empty product card should render in outfit list when empty', () => {
   });
 });
