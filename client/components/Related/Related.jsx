56293d55481fbb6a3d685aac17a07d03719f8eab
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-11-05 03:39:51 -0700

    Picked starting item with >4 related products

--- client/components/Related/Related.jsx
+++ client/components/Related/Related.jsx
@@ -5,21 +5,19 @@ import RelatedProducts from './RelatedProducts.jsx';
 import Outfit from './Outfit.jsx';
 
 const Related = (props) => {
-  const [productId, setProductId] = useState(props.product);
+  const [productId, setProductId] = useState(props.productId);
   const [relatedProducts, setRelatedProducts] = useState([]);
   const [outfitList, setOutfitList] = useState([]);
 
-  const getRelated = props.mock ? props.mock : (() => {
-    axios.post('/related', { product: props.product })
+  useEffect(() => {
+    axios.post('/related', { product: props.productId })
       .then((result) => {
         setRelatedProducts(result.data);
       })
       .catch((error) => {
         console.log('Client unable to get related products: ', error);
       });
-  });
-
-  useEffect(getRelated, []);
+  }, [props.productId]);
 
   const handleAction = (event) => {
     const target = event.target.tagName === 'I' ? event.target : event.target.firstElementChild;
