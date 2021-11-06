547992ae94084ef9680514dd041c737b2117a3dd
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-11-06 11:21:11 -0700

    Fixed features on fixture

--- test/fixtures.js
+++ test/fixtures.js
@@ -4,7 +4,11 @@ const fixtures = {
       id: 60012,
       category: 'Suit',
       name: 'Abigale Suit',
-      features: [ { feature: 'Cut', value: '"Striaght"' } ],
+      features: [
+        {feature: 'Frame', value: '"DuraResin"'},
+        {feature: 'Non-GMO', value: null},
+        {feature: 'Non-GMO', value: null}
+      ],
       thumbnailUrl: 'https://images.unsplash.com/photo-1517278322228-3fe7a86cf6f0?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80',
       defaultPrice: '896.00',
       originalPrice: '896.00',
@@ -14,7 +18,7 @@ const fixtures = {
       id: 60389,
       category: 'Sweater',
       name: 'Alberta Sweater',
-      features: [ { feature: 'Fabric', value: '"Cashmere"' } ],
+      features: [ { feature: 'Stitching', value: '"Cross Stitch"' } ],
       thumbnailUrl: 'https://images.unsplash.com/photo-1554774853-d50f9c681ae2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
       defaultPrice: '595.00',
       originalPrice: '595.00',
@@ -24,7 +28,11 @@ const fixtures = {
       id: 60123,
       category: 'Dress',
       name: 'The Evangeline Dress',
-      features: [ { feature: 'Stitching', value: '"Cross Stitch"' } ],
+      features: [
+        { feature: 'Sustainably Sourced', value: null },
+        { feature: 'Fair Trade Certified', value: null },
+        { feature: '5 Year Warranty', value: null }
+      ],
       thumbnailUrl: 'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
       defaultPrice: '230.00',
       originalPrice: '230.00',
@@ -35,8 +43,9 @@ const fixtures = {
       category: 'Trousers',
       name: 'Julian Trousers',
       features: [
-        { feature: 'Green Leaf Certified', value: null },
-        { feature: 'Lens', value: '"Ultrasheen Basic"' }
+        { feature: 'Non-GMO', value: null },
+        { feature: 'Frame', value: '"AllLight Composition Resin"' },
+        { feature: 'Stitching', value: '"Double Stitch"' }
       ],
       thumbnailUrl: 'https://images.unsplash.com/photo-1422557379185-474fa15bf770?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
       defaultPrice: '881.00',
@@ -47,11 +56,7 @@ const fixtures = {
       id: 60109,
       category: 'Shorts',
       name: 'Ezekiel Shorts',
-      features: [
-        { feature: 'Sustainably Sourced', value: null },
-        { feature: 'Fair Trade Certified', value: null },
-        { feature: '5 Year Warranty', value: null }
-      ],
+      features: [ {feature: 'Cut', value: '"Striaght"'} ],
       thumbnailUrl: 'https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
       defaultPrice: '906.00',
       originalPrice: '1024.00',
@@ -62,9 +67,8 @@ const fixtures = {
       category: 'Suit',
       name: 'Raymundo Suit',
       features: [
-        { feature: 'Frame', value: '"DuraResin"' },
-        { feature: 'Non-GMO', value: null },
-        { feature: 'Non-GMO', value: null }
+        {feature: 'Green Leaf Certified', value: null},
+        {feature: 'Lens', value: '"Ultrasheen Basic"'}
       ],
       thumbnailUrl: 'https://images.unsplash.com/photo-1560857792-215f9e3534ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80',
       defaultPrice: '352.00',
@@ -75,11 +79,7 @@ const fixtures = {
       id: 59772,
       category: 'Coat',
       name: 'Otho Coat',
-      features: [
-        { feature: 'Non-GMO', value: null },
-        { feature: 'Frame', value: '"AllLight Composition Resin"' },
-        { feature: 'Stitching', value: '"Double Stitch"' }
-      ],
+      features: [ {feature: 'Fabric', value: '"Cashmere"'} ],
       thumbnailUrl: 'https://images.unsplash.com/photo-1469617833234-8a6843da14d0?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80',
       defaultPrice: '400.00',
       originalPrice: '400.00',
