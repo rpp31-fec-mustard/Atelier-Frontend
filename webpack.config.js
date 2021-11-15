d844e09474e29424365f955bf68e53c6e3781a54
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-11-14 20:44:21 -0800

    Removed compression plugin from Webpack; not working. Activated source-map so react-tracking would work

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const CompressionPlugin = require('compression-webpack-plugin');
-
 const entry = __dirname + '/client/components/index.jsx';
 
 module.exports = {
@@ -8,9 +6,6 @@ module.exports = {
   optimization: {
     usedExports: true,
   },
-  plugins: [new CompressionPlugin({
-    compressionOptions: {level: 9}
-  })],
   entry: ['regenerator-runtime/runtime.js', entry],
   module: {
     rules: [
@@ -39,4 +34,5 @@ module.exports = {
     filename: 'bundle.js',
     path: path.join(__dirname, '/client/public')
   },
+  devtool: 'source-map'
 };
