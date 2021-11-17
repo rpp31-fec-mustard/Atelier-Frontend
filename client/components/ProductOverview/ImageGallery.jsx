7eed5026550412c37bfddbafb72724fee17864c1
Michael Lapid <19236670+addessosf@users.noreply.github.com>
2021-11-17 01:53:10 -0800

    Huge ugly refactor of main image so it can have an alt
    That was bad. see journal.

--- client/components/ProductOverview/ImageGallery.jsx
+++ client/components/ProductOverview/ImageGallery.jsx
@@ -42,52 +42,57 @@ const ImageGallery = ({currentStyle, productId, productName}) => {
     setIndex(0);
   }, [productId]);
 
+  const handleClickImage = () => {
+    setShow(true);
+  };
+
 
   //needed because useEffect above is not resolved in time
   //when productId changes and new product is fetched, seems like the render below is resolved
   //before useEffect resets the index to 0.
   if (photos[index]) {
-    let node = document.getElementsByClassName('image_gallery_po').style;
-    mlog(logC + ' node', node);
+    // let node = document.getElementsByClassName('image_gallery_po').style;
+    // // mlog(logC + ' node', node);
 
     return (
-      <div className='image_gallery_po' style={{
-        color: 'red',
-        backgroundImage: `url(${photos[index].url})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover'
-      }}>
-        <ThumbnailsBar
-          photos={photos}
-          handleThumbnailClick={(index) => { setIndex(index); }}
-          altText = {altText} />
-        <div className='arrow_po'>
-          <div className='arrow_space_po'></div>
-          <ArrowLeft
-            imageLeftClick={imageLeftClick}
-            index={index} />
-          <div className='arrow_space_po'></div>
-        </div>
-        <div className='space01_po'></div>
-        <FullScreenModal
-          currentStyle={currentStyle}
-          productName={productName}
-          index={index}
-          show={show}
-          onClose={() => setShow(false)}/>
-        <div className='arrow_po'>
-          <div className='arrow_space_po'>
-            <button
+      <React.Fragment>
+
+        <div className='image_container_po'>
+          <img className='main_image_po' src={photos[index].url} alt='Main Image'/>
+          <div className='image_gallery_po'>
+            <ThumbnailsBar
+              photos={photos}
+              handleThumbnailClick={(index) => { setIndex(index); }}
+              altText = {altText} />
+            <div className='arrow_po'>
+              <div className='arrow_space_po' onClick={handleClickImage}></div>
+              <ArrowLeft
+                imageLeftClick={imageLeftClick}
+                index={index} />
+              <div className='arrow_space_po' onClick={handleClickImage}></div>
+            </div>
+            <div className='space01_po' onClick={handleClickImage}></div>
+            <FullScreenModal
+              currentStyle={currentStyle}
+              productName={productName}
+              index={index}
+              show={show}
+              onClose={() => setShow(false)}/>
+            <div className='arrow_po'>
+              <div className='arrow_space_po' onClick={handleClickImage}>
+                {/* <button
               className='hover_fullscreen_button_po'
-              onClick={() => setShow(true)}>H</button>
+              onClick={handleClickImage}>H</button> */}
+              </div>
+              <ArrowRight
+                imageRightClick={imageRightClick}
+                index={index}
+                indexMax={photos.length - 1}/>
+              <div className='arrow_space_po' onClick={handleClickImage}></div>
+            </div>
           </div>
-          <ArrowRight
-            imageRightClick={imageRightClick}
-            index={index}
-            indexMax={photos.length - 1}/>
-          <div className='arrow_space_po'></div>
         </div>
-      </div>
+      </React.Fragment>
     );
   } else {
     //needed to set index to 0 if this conditional
