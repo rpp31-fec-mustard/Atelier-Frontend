4b00050dce558f040f886ff11eb11aaaf45ea1f2
xoxohorses <69382434+xoxohorses@users.noreply.github.com>
2021-11-17 20:42:08 -0800

    Main submodule render tests done

--- test/Related.test.js
+++ test/Related.test.js
@@ -7,45 +7,94 @@ import axios from 'axios';
 // components
 import Related from '../client/components/Related/Related.jsx';
 import RelatedProducts from '../client/components/Related/RelatedProducts.jsx';
+import Outfit from '../client/components/Related/Outfit.jsx';
 
 // fixtures
 import * as fixtures from './fixtures.js';
 
-// TODO: test that spy was called
-
 /* TESTS */
 
 describe('Related products module: render tests', () => {
+  beforeEach(() => {
+    jest.spyOn(axios, 'post').mockImplementation((endpoint, options) => {
+      switch (endpoint) {
+      case '/related':
+        return Promise.resolve({ data: fixtures.default.relatedProducts });
+      default:
+        const error = new Error(`ENDPOINT[${endpoint}] MUST BE MOCKED`);
+        console.error({ error });
+      }
+    });
+  });
+
   describe('<RelatedProducts />', () => {
-    beforeEach(() => {
-      jest.spyOn(axios, 'post').mockImplementation((endpoint, options) => {
-        switch (endpoint) {
-        case '/related':
-          return Promise.resolve({ data: fixtures.relatedProducts });
-        default:
-          const error = new Error(`ENDPOINT[${endpoint}] MUST BE MOCKED`);
-          console.error({ error });
-        }
-      });
+    test('<RelatedProducts /> should render', () => {
+      const related = shallow(<Related productId={'59601'}/>);
+      expect(related.children().childAt(0).name()).toBe('RelatedProducts');
     });
 
-    test('<RelatedProducts /> should receive a list of relatedProducts on render', () => {
-      withHooks(() => {
-        const related = shallow(<Related productId={'59601'}/>);
-        console.log(related.debug());
-        return Promise.resolve().then(() => {
-          expect(related.find(RelatedProducts).props().relatedProducts).toBe(fixtures.relatedProducts);
-        });
-      });
+    test('<RelatedProducts /> should render no cards if no relatedProducts', () => {
+      const relatedProducts = shallow(<RelatedProducts
+        productId={'59601'}
+        relatedProducts={[]}
+        handleAction={()=>{}}
+        handleScroll={{handleLeftScroll: ()=>{}, handleRightScroll: ()=>{}}}
+        checkScrollPosition={()=>{}}
+        homeProduct={{}}
+        renderRelated={()=>{}}
+      />);
+
+      expect(relatedProducts.find('.prod-cards-wrapper').children()).toHaveLength(0);
     });
+
+    test('<RelatedProducts /> should render a list of relatedProducts cards', () => {
+      const relatedProducts = shallow(<RelatedProducts
+        productId={'59601'}
+        relatedProducts={fixtures.default.relatedProducts}
+        handleAction={()=>{}}
+        handleScroll={{handleLeftScroll: ()=>{}, handleRightScroll: ()=>{}}}
+        checkScrollPosition={()=>{}}
+        homeProduct={{}}
+        renderRelated={()=>{}}
+      />);
+
+      expect(relatedProducts.find('.prod-cards-wrapper').children()).toHaveLength(7);
+    });
+
   });
 
-  test('Related products and Outfit submodules should render', () => {
-  });
+  describe('<Outfit />', () => {
 
-  test('At most four product cards should render in related products submodule', () => {
-  });
+    test('<Outfit /> should render', () => {
+      const related = shallow(<Related productId={'59601'}/>);
+      expect(related.children().childAt(1).name()).toBe('Outfit');
+    });
 
-  test('Empty product card should render in outfit list when empty', () => {
+    test('<Outfit /> should render "Add product" card if empty outfitList', () => {
+      const outfit = shallow(<Outfit
+        outfitList={[]}
+        handleAction={()=>{}}
+        handleScroll={{handleLeftScroll: ()=>{}, handleRightScroll: ()=>{}}}
+        checkScrollPosition={()=>{}}
+        renderRelated={()=>{}}
+      />);
+
+      expect(outfit.find('.add-product')).toBeTruthy();
+    });
+
+    test('<Outfit /> should render "Add product" card if empty outfitList', () => {
+      const outfit = shallow(<Outfit
+        outfitList={fixtures.default.outfitList}
+        handleAction={()=>{}}
+        handleScroll={{handleLeftScroll: ()=>{}, handleRightScroll: ()=>{}}}
+        checkScrollPosition={()=>{}}
+        renderRelated={()=>{}}
+      />);
+
+      // console.log(outfit.debug());
+
+      expect(outfit.find('.add-product')).toEqual({});
+      expect(outfit.find('.prod-cards-wrapper').children()).toHaveLength(2);
+    });
   });
 });
