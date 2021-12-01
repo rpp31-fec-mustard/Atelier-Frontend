cf22eaa0034c8a319470eb2d9f65a431bc32b908
Michael Lapid <19236670+addessosf@users.noreply.github.com>
2021-12-01 00:00:03 -0800

    complete outfitList button css

--- client/components/ProductOverview/StyleCartSubs/AddOutfitListButton.jsx
+++ client/components/ProductOverview/StyleCartSubs/AddOutfitListButton.jsx
@@ -1,16 +1,17 @@
 import React, {useState, useEffect} from 'react';
 
 
-
 const AddOutfitListButton = ({toggleProductToOutfitList, isProductInOutfitList}) => {
 
+  let starIcon = <i className="ri-star-line"></i>;
   let favoriteStatus = isProductInOutfitList ? 'favorites_on_po' : 'favorites_off_po';
-
 
   return (
     <React.Fragment>
       <div className={`favorites_add_button_po ${favoriteStatus}`}
-        onClick={toggleProductToOutfitList}>favorite</div>
+        onClick={toggleProductToOutfitList}>
+        {starIcon}
+      </div>
     </React.Fragment>
   );
 };
